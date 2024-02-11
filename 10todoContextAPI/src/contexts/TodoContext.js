28902b2279b0e380411341c2d7723014519f7f7a
smdekate <smdekate@gmail.com>
2024-02-11 16:28:56 +0530

    done with context API with todos Project

---
+++ 10todoContextAPI/src/contexts/TodoContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [{}],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => useContext(TodoContext)
+
+export const TodoProvider = TodoContext.Provider
